9fb5c42eb1f437d8c1b043b17e9e81790099d7ab
Sergio <sergio.hernandez@littlestarmedia.com>
2021-07-21 11:22:41 +0100

    frontend dockerized and connected with backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import subscriptionPlans from './routes/subscription-plans';
 
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/', subscriptionPlans);
---
+++ frontend/src/App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const testRequest = () => {
+    axios.get('http://localhost:5000/subscription-plans')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }
+  testRequest();
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
